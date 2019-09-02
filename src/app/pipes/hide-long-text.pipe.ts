4a1e28a47e16bda467e9361712e673400135bb09
杨瑞奇 <grandruiqi@hotmail.com>
2019-09-02 01:48:52 -0400

    change button color, increase long text length

--- src/app/pipes/hide-long-text.pipe.ts
+++ src/app/pipes/hide-long-text.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class HideLongTextPipe implements PipeTransform {
 
   transform(value: string): string {
-    return value.length > 20 ? value.substr(0, 20) + "..." : value
+    return value.length > 25 ? value.substr(0, 25) + "..." : value
   }
 
 }
