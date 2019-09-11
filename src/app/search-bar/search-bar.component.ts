14cf4016b4bd1418480c0262c6da923fbc4a5cdf
杨瑞奇 <grandruiqi@hotmail.com>
2019-09-11 11:27:33 -0400

    add service testing

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { AlbumService } from '../services/album.service';
+import { Component, OnInit } from '@angular/core'
+import { AlbumService } from '../services/album.service'
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss']
 })
-export class SearchBarComponent implements OnInit {
-  searchTerm: string;
+export class SearchBarComponent {
+  searchTerm: string
 
   constructor(private albumService: AlbumService) {}
 
-  ngOnInit() {}
-
   onSearch() {
-    this.albumService.getAlbums(this.searchTerm);
+    this.albumService.getAlbums(this.searchTerm)
   }
 }
--- src/app/services/album.service.spec.ts
+++ src/app/services/album.service.spec.ts
@@ -1,14 +1,30 @@
 import { AlbumService } from './album.service'
-import { TestBed } from '@angular/core/testing'
-import { HttpClientModule } from '@angular/common/http'
+import { TestBed, fakeAsync, tick } from '@angular/core/testing'
+import { HttpClient } from '@angular/common/http'
 import { initialValue } from './album.service'
+import { of } from 'rxjs'
+import { delay } from 'rxjs/operators'
+
+const params = {
+  delayTime: 2000,
+  tickTime: 4000
+}
+
+const HttpClientMock = {
+  get: () => of({ results: [1, 2, 3] }).pipe(delay(params.delayTime))
+}
 
 describe('AlbumService', () => {
   let service: AlbumService
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AlbumService],
-      imports: [HttpClientModule]
+      providers: [
+        AlbumService,
+        {
+          provide: HttpClient,
+          useValue: HttpClientMock
+        }
+      ]
     })
     service = TestBed.get(AlbumService)
   })
@@ -24,4 +40,26 @@ describe('AlbumService', () => {
     album$.subscribe(data => (albumInitialState = data))
     expect(albumInitialState).toEqual(expectedValue)
   })
+
+  it(`should have a getAlbums method take one param (artist name) and return albums`, fakeAsync(() => {
+    let albumState
+    let isFetchingState
+    const album$ = service.albums
+    const isFetching$ = service.isFetchingAlbums
+
+    album$.subscribe(data => {
+      albumState = data
+    })
+    isFetching$.subscribe(data => {
+      isFetchingState = data
+    })
+
+    expect(isFetchingState).toBe(null)
+    service.getAlbums('test')
+    expect(isFetchingState).toBe(true)
+    tick(params.tickTime)
+    expect(isFetchingState).toBe(false)
+    console.log(albumState)
+    expect(albumState).toEqual([1, 2, 3])
+  }))
 })
